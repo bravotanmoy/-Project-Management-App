b17f1b5f5a5bc36273195814a3787985cfe18f25
Tanmoy Biswas <tanmoyb.cse@gmail.com>
2025-01-22 09:27:15 +0600

    Add some text

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -68,19 +68,5 @@ export default function NewProject({onAdd, onCancel}) {
         </>
       );
 
-    // return (
-    //     <div className="w-[35rem]">
-    //     <menu className="flex items-center justify-end gap-4 my-4">
-    //         <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
-    //         <li><button className="px-6 py-2 text-stone-800 text-stone-50 hover:text-stone-950" onClick={handleSave}>Save</button></li>
-    //     </menu>
-    //     <div>
-
-    //         <Input type="text" ref={title} label="Title" />
-    //         <Input ref={description} label="Description" textarea />
-    //         <Input type="date" ref={dueDate} label="Due Date" />
-    //     </div>
-    // </div>
-    // );
 
 }
